b498dfad1942becf415c3fcf442545dfa84acfef
Pongsathorn1997 <59756685+Pongsathorn1997@users.noreply.github.com>
2021-03-03 22:25:44 +0700

    adding Petimist word and font

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,19 +2,18 @@
     <v-app id="homepage">
       <section style="height: 100vh">
         <v-parallax
-            dark
             src="../assets/background.jpg"
             style="height: 100vh"
         >
-          <v-container fluid fill-height>
-            <v-layout justify-center>
-              <v-img
-                  :aspect-ratio="1"
-                  :max-width="350"
-                  :max-height="350"
-                  src="../assets/circle.png"
-              >
-                <v-layout justify-center>
+          <v-layout justify-center>
+            <v-img
+                class="mt-5"
+                :aspect-ratio="1"
+                :max-width="350"
+                :max-height="350"
+                src="../assets/circle.png"
+            >
+              <v-layout justify-center>
                 <v-img
                     class="mt-6"
                     :aspect-ratio="1"
@@ -23,11 +22,27 @@
                     src="https://media.tenor.com/images/b6816283f39bdc3b46c80a122a603456/tenor.gif"
                 >
                 </v-img>
-                </v-layout>
-              </v-img>
+              </v-layout>
+            </v-img>
+          </v-layout>
+          <v-container fluid fill-height>
+            <v-layout justify-center>
+              <v-flex xs12 sm8 md4>
+                <h1 class="black--text MyFont"> Petimist </h1>
+              </v-flex>
             </v-layout>
           </v-container>
         </v-parallax>
       </section>
     </v-app>
 </template>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap');
+
+.MyFont{
+  font-size: 8em;
+  font-family: 'Architects Daughter', cursive
+}
+</style>
