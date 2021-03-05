76bd89de752fd559b7686644573a8e31d7d2e2a2
max <sumeth.sir@student.mahidol.edu>
2021-03-05 16:33:21 +0700

    update vuex for authentication

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,36 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: null,
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
   },
   actions: {
+    setUserAction(context, payload) {
+      context.commit('setUser', payload);
+    },
+    signInAction(context, payload) {
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then((userCredential) => {
+          // Signed in
+          const { user } = userCredential;
+          // ...
+          context.commit('setUser', user);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
   modules: {
   },
