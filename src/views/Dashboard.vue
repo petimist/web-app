53c6246be9cc622674dedc5679d0684d976eeb79
Pongsathorn1997 <59756685+Pongsathorn1997@users.noreply.github.com>
2021-03-06 14:14:20 +0700

    adding v-bar, logo in dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>This is Dashboard Page</h1>
+  <v-app>
+    <v-form>
+      <v-app-bar dense color="#FFD180">
+        <v-img
+            class="mr-3 mb-3"
+            :aspect-ratio="1"
+            :max-width="45"
+            :max-height="45"
+            src="https://media.tenor.com/images/b6816283f39bdc3b46c80a122a603456/tenor.gif"
+        ></v-img>
+        <h1 class="MyFont2">Petimist</h1>
+      </v-app-bar>
+    </v-form>
+  </v-app>
 </template>
 
 <script>
@@ -8,6 +21,11 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap');
 
+.MyFont2{
+  font-size: 1cm;
+  font-family: 'Architects Daughter', cursive
+}
 </style>
