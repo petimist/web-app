10aa2631ad312bf7bc5b9d2216033a6656a911bd
parmcoder <possawat2017@gmail.com>
2021-03-06 20:59:20 +0700

    dotenv fixed

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,18 +1,20 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+require('dotenv').config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// console.log(process.env);
 
 // Use dot env later
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID,
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID,
+  measurementId: process.env.VUE_APP_MEASUREMENTID,
 };
 // Initialize Firebase
 
