26560687d1cfc779cb36dc458d0c96b62db35c93
max <sumeth.sir@student.mahidol.edu>
2021-03-07 00:17:40 +0700

    update firebase for dotenv

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -7,13 +7,13 @@ import store from '../store/index';
 
 // Use dot env later
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID,
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID,
+  measurementId: process.env.VUE_APP_MEASUREMENTID,
 };
 // Initialize Firebase
 
