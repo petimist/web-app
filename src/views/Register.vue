86f9304f3be58cf01468b499661f8f7ea50764be
parmcoder <possawat2017@gmail.com>
2021-03-08 14:54:47 +0700

    register all done

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,84 +3,87 @@
     background: url('https://image.freepik.com/free-photo/abstract-blur-light-gradient-pink-soft-pastel-yellow-wallpaper-background_7636-1347.jpg');
     background-size: cover
   ">
-  <v-form @submit.prevent="submitRegister">
-    <v-card-title class="justify-center mt-8">
-      <h1 class=" black--text MyFont2">Register</h1>
-    </v-card-title>
+    <v-form @submit.prevent="submitRegister">
+      <v-card-title class="justify-center mt-8">
+        <h1 class=" black--text MyFont2">Register</h1>
+      </v-card-title>
 
-    <v-container class="mt-5" align-center justify-center>
-      <v-layout align-center justify-center>
-        <v-col
+      <v-container
+        class="mt-5"
+        align-center
+        justify-center
+      >
+        <v-layout
+          align-center
+          justify-center
+        >
+          <v-col
             cols="18"
             sm="11"
             md="6"
-        >
-          <v-text-field
-              label="Name"
-              v-model="name"
-              background-color="white"
-              outlined color="red"
-              type="name"
-              hint="Please enter your name"
-          ></v-text-field>
-
-          <v-text-field
+          >
+            <v-text-field
               label="Email"
               v-model="email"
               background-color="white"
-              outlined color="red"
+              outlined
+              color="red"
               type="email"
               hint="Please enter your email"
               :error-messages="emailErrors"
-          ></v-text-field>
+            ></v-text-field>
 
-          <v-text-field
+            <v-text-field
               label="Password"
               v-model="password"
               background-color="white"
-              outlined color="red"
+              outlined
+              color="red"
               type="password"
               hint="Please enter your password"
               :error-messages="passwordErrors"
-          ></v-text-field>
+            ></v-text-field>
 
-          <v-text-field
+            <v-text-field
               label="ConfirmPassword"
               v-model="confirmPassword"
               background-color="white"
-              outlined color="red"
+              outlined
+              color="red"
               type="password"
               hint="Confirm your password"
               :error-messages="repeatedPasswordErrors"
-          ></v-text-field>
+            ></v-text-field>
 
-          <v-checkbox
-              v-model="selected"
+          </v-col>
+        </v-layout>
+        <v-container class="justify-center">
+          <v-col>
+            <v-btn
+              color="#FFD180"
+              class="mr-10 MyFont2"
+              @click="backToLogin"
+            >
+              <v-icon
+                dark
+                left
+              >
+                mdi-arrow-left
+              </v-icon>
+              Back to login
+            </v-btn>
+
+            <v-btn
+              color="#FFD180"
               class="MyFont2"
-              label="Agree to register"
-              color="success"
-              value="success"
-              hide-details
-              :error-messages="checkBoxErrors"
-          ></v-checkbox>
-        </v-col>
-      </v-layout>
-      <v-container class="justify-center">
-      <v-col>
-        <v-btn color="#FFD180" class="mr-10 MyFont2" @click="backToLogin">
-          <v-icon dark left>
-            mdi-arrow-left
-          </v-icon>
-          Back to login
-        </v-btn>
-
-        <v-btn color="#FFD180" class="MyFont2" type="submit" @click="submitRegister">
-          Submit
-        </v-btn>
-      </v-col>
+              type="submit"
+            >
+              Submit
+            </v-btn>
+          </v-col>
+        </v-container>
       </v-container>
-    </v-container>
-  </v-form>
+    </v-form>
   </v-app>
 </template>
 
@@ -129,7 +132,8 @@ export default {
       // eslint-disable-next-line no-unused-expressions
       !this.$v.password.required && errors.push('Password is required');
       // eslint-disable-next-line no-unused-expressions
-      !this.$v.password.minLength && errors.push('Password must have atleast 6 character');
+      !this.$v.password.minLength
+        && errors.push('Password must have atleast 6 character');
       return errors;
     },
     repeatedPasswordErrors() {
@@ -138,14 +142,16 @@ export default {
       // eslint-disable-next-line no-unused-expressions
       !this.$v.confirmPassword.required && errors.push('Confirm your password');
       // eslint-disable-next-line no-unused-expressions
-      !this.$v.confirmPassword.sameAsPassword && errors.push('Password does not match');
+      !this.$v.confirmPassword.sameAsPassword
+        && errors.push('Password does not match');
       return errors;
     },
     checkBoxErrors() {
       const errors = [];
       if (!this.$v.selected.$dirty) return errors;
       // eslint-disable-next-line no-unused-expressions
-      !this.$v.selected.required && errors.push('Must agree to register(Tick checkbox)');
+      !this.$v.selected.required
+        && errors.push('Must agree to register(Tick checkbox)');
       return errors;
     },
   },
@@ -156,15 +162,9 @@ export default {
         firebase
           .auth()
           .createUserWithEmailAndPassword(this.email, this.password)
-          .then((data) => {
+          .then(() => {
+            this.$store.dispatch('signOutAction');
             this.$router.push('/login');
-            data.user
-              .then(() => {
-                this.RegisterSuccess = 'Register Successful! Proceed to Dashboard';
-                this.RegisterError = null;
-                this.message = 'Go to Dashboard';
-                this.$store.dispatch('auth/userRegister', data);
-              });
           })
           .catch((err) => {
             console.log(err.message);
@@ -182,11 +182,10 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap");
 
-@import url('https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap');
-
-.MyFont2{
+.MyFont2 {
   font-size: 3em;
-  font-family: 'Architects Daughter', cursive
+  font-family: "Architects Daughter", cursive;
 }
 </style>
