668b561ea8a6b749d343f1ba5c2664ac62f9a3d9
parmcoder <possawat2017@gmail.com>
2021-03-23 13:45:51 +0700

    start fixing bug on appointment

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,8 @@
 <template>
   <div id="app">
-      <v-navigation-drawer
-          app
-          clipped
-          color="#BDBDBD"
-          width="200"
-      >
-        <v-list class="black--text">
-        <v-list-item
-            exact v-for="link of links"
-            :key="link.title"
-            :to="link.to"
-        >
+    <v-navigation-drawer app clipped color="#BDBDBD" width="200">
+      <v-list class="black--text">
+        <v-list-item exact v-for="link of links" :key="link.title" :to="link.to">
           <v-list-item-action>
             <v-icon>mdi-{{ link.icon }}</v-icon>
           </v-list-item-action>
@@ -19,14 +10,14 @@
             <v-list-item-title> {{ link.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      </v-list>
+    </v-navigation-drawer>
 
-        <!-- Provides the application the proper gutter -->
-        <v-container fluid>
-          <!-- If using vue-router -->
-          <router-view></router-view>
-        </v-container>
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
   </div>
 </template>
 
@@ -66,9 +57,8 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap');
 
-.MyFont2{
+.MyFont2 {
   font-size: 1cm;
-  font-family: 'Architects Daughter', cursive
+  font-family: 'Architects Daughter', cursive;
 }
-
 </style>
